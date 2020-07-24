04d3d2d2c667a44a355738ecc26a500991b3674e
Harry JongSeok Lee <lodossw@gmail.com>
2020-07-25 03:04:48 +0900

    초기커밋

---
+++ SUMMARY.md
@@ -0,0 +1,16 @@
+# Summary
+
+* [소개](README.md)
+
+## 들어가기  
+
+* [프로그램 언어의 이해](pages/lec01/01.md)
+* [VS2019 다운로드 및 설치](pages/lec01/02.md)
+* [VS2019 기본사용법](pages/lec01/03.md)
+* [C언어의 기본구조](pages/lec01/04.md)
+* [Git을 이용한 소스관리 기초](pages/lec01/05.md)
+
+
+
+
+
---
+++ publish.sh
@@ -0,0 +1,16 @@
+gitbook install && gitbook build
+
+git checkout gh-pages
+
+git pull origin gh-pages --rebase
+
+cp -R _book/* .
+
+git clean -fx node_modules
+git clean -fx _book
+
+git add .
+
+git commit -a -m "Publish Doc"
+git push origin gh-pages
+git checkout master
